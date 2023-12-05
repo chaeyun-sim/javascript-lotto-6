5521847a54ac03999a7b41eb4786cbe30e865784
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-12-05 14:43:23 +0900

    ✨ feat: InputView, OutputView 생성

---
+++ src/View/InputView.js
@@ -0,0 +1,27 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { GUIDE_TEXT } from '../constants/constants.js';
+
+const InputView = {
+  async inputMoney() {
+    const input = await MissionUtils.Console.readLineAsync(
+      GUIDE_TEXT.inputMoney
+    );
+    return String(input);
+  },
+
+  async inputWinningNumbers() {
+    const input = await MissionUtils.Console.readLineAsync(
+      GUIDE_TEXT.inputWinningNumbers
+    );
+    return String(input);
+  },
+
+  async inputBonusNumber() {
+    const input = await MissionUtils.Console.readLineAsync(
+      GUIDE_TEXT.inputBonusNumber
+    );
+    return String(input);
+  },
+};
+
+export default InputView;
---
+++ src/View/OutputView.js
@@ -0,0 +1,24 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { GUIDE_TEXT } from '../constants/constants.js';
+
+const OutputView = {
+  print(text) {
+    MissionUtils.Console.print(text);
+  },
+
+  printLottos(text, lottos) {
+    MissionUtils.Console.print(GUIDE_TEXT.printLotto);
+    lottos.forEach(lotto => MissionUtils.Console.print(lotto));
+  },
+
+  printStats() {
+    MissionUtils.Console.print(GUIDE_TEXT.winningState);
+    MissionUtils.Console.print(GUIDE_TEXT.divider);
+  },
+
+  printReturnRate(rate) {
+    MissionUtils.Console.print(GUIDE_TEXT.returnRate.replace('*', rate));
+  },
+};
+
+export default OutputView;
